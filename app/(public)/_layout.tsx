f4aeaba8abc36294968753a0027e61ad5d33bd2c
Lakusan <47719054+Lakusan@users.noreply.github.com>
2026-02-15 15:18:14 +0100

    fallback to js tabs; waiting for sdk 55+

--- app/(public)/_layout.tsx
+++ app/(public)/_layout.tsx
@@ -2,9 +2,8 @@ import { Button } from "@/components/ui/button";
 import { BREAKPOINTS } from "@/src/constants/ui/breakpoints";
 import { useAuthFlow } from '@/src/contexts/use-auth-flow-context';
 import Feather from "@expo/vector-icons/Feather";
-import { Stack, Tabs } from "expo-router";
+import { Tabs } from "expo-router";
 import { Drawer } from "expo-router/drawer";
-import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 import React from "react";
 import { Platform, useWindowDimensions } from "react-native";
 
@@ -85,7 +84,6 @@ export default function PublicLayout() {
   }
 
   // SMARTPHONE + TABLET Dimensions on Web → Expor/RN Tabs
-  if (isWeb) {
 
     return (
       <Tabs
@@ -153,59 +151,3 @@ export default function PublicLayout() {
       </Tabs>
     );
   }
-  // Sollte IOS native stuff zurueck geben, aber IOS Glass icons muessen wohl betachtet werden
-  if (isIOS) {
-
-    return (
-      <></>
-      // <NativeTabs
-      // labelStyle={{
-      //   // For the text color
-      //   color: DynamicColorIOS({
-      //     dark: 'white',
-      //     light: 'black',
-      //   }),
-      // }}
-      // // For the selected icon color
-      // tintColor={DynamicColorIOS({
-      //   dark: 'white',
-      //   light: 'black',
-      // })}>
-      //   <NativeTabs.Trigger name="index">
-      //     <Icon sf={{ default: 'house', selected: 'house.fill' }} drawable="custom_home_drawable" />
-      //   </NativeTabs.Trigger>
-      //   <NativeTabs.Trigger name="settings">
-      //     <Icon sf={{ default: 'gearshape', selected: 'gearshape.fill' }} drawable="custom_settings_drawable" />
-      //   </NativeTabs.Trigger>
-      // </NativeTabs>
-    );
-  }
-  // Native TabBar fuer Android
-  // sf IOS Icons direkt vom iOS 
-  // drawable Android Icons aus android/app/src/res/drawable
-  // Von googles Material Design Icons -> Selbst ablegen, daher android/app/src/res/drawable/<MaterialIconName>*.xml -> Github
-  // liegen aktuell in assets/android -> kopieren in android/app/src/res/drawable nach repbuild --clean
-  // Nur angelegte Routen werden angezeigt und gerendert; keine danebenliegenden wie mit RN Tabs
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-    <NativeTabs >
-      <NativeTabs.Trigger name="index">
-        <Label>Home</Label>
-           <Icon drawable="home_24px" />
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="add">
-        <Icon drawable="add_24px" />
-        <Label>Add</Label>
-      </NativeTabs.Trigger>
-            <NativeTabs.Trigger name="search">
-        <Icon drawable="search_24px" />
-        <Label>Search</Label>
-      </NativeTabs.Trigger>
-            <NativeTabs.Trigger name="settings">
-        <Icon drawable="settings_account_box_24px" />
-        <Label>Settings</Label>
-      </NativeTabs.Trigger>
-    </NativeTabs>
-    </Stack>
-  )
-}
