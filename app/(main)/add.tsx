50bc16143f7cc32b6baf33fc1cc3b028e9377bcb
47719054+Lakusan@users.noreply.github.com <47719054+Lakusan@users.noreply.github.com>
2026-05-13 13:31:47 +0200

    add Pattern basic layout

--- app/(main)/add.tsx
+++ app/(main)/add.tsx
@@ -1,7 +1,49 @@
-import { Text } from '@/components/ui/text';
+import RadioButton from "@/src/components/buttons/RadioButton";
+import React, { ReactNode } from "react";
+import { ScrollView, Text, View } from "react-native";
 
-export default function Add(){
-    return(
-        <Text>MAIN: add</Text>
-    );
+
+type SectionVariant = "textInput" | "radiobutton" | "form" | "list";
+
+interface SectionCardProps {
+  label: string;
+  children: ReactNode;
+  variant?: SectionVariant;
+}
+
+function SectionCard({ label, children, }: SectionCardProps) {
+    // Hier dann switch einbauen wenn ich varianten einbauen will
+  return (
+    <View className="bg-pink-500">
+      
+      <Text className="text-xs font-semibold pt-1 pl-1">
+        {label}
+      </Text>
+      <View className="bg-white m-2">
+        {children}
+      </View>
+    </View>
+  );
+}
+
+export default function Add() {
+  return (
+    <ScrollView className="flex-1 flex-col bg-red-500">
+      <Text>MAIN: add</Text>
+
+      <View className="bg-green-500 flex min-h-[400px]">
+        <SectionCard label="string">
+        <RadioButton id={""}></RadioButton>
+        </SectionCard>
+      </View>
+
+      <View className="bg-yellow-500 flex min-h-10">
+        <Text>Container 2</Text>
+      </View>
+
+      <View className="bg-blue-500 flex-3">
+        <Text>Container 3</Text>
+      </View>
+    </ScrollView>
+  );
 }
